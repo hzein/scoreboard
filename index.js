e603beba09cce3b466a6e3f6e5b038e0bbfee716
hzein <hzein286@gmail.com>
2022-08-26 14:56:44 -0700

    Commit initial files

---
+++ index.js
@@ -0,0 +1,30 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+let homeCount = 0
+let guestCount = 0
+
+function addScore(type, num){
+    if (type === 'home'){
+        homeCount += num
+        homeScore.innerText = homeCount
+    } else {
+        guestCount += num
+        guestScore.innerText = guestCount
+    }
+    if(homeCount > guestCount){
+        homeScore.style.boxShadow= "0 0 0 1px red"
+        guestScore.style.boxShadow = "none"
+    } else if (homeCount < guestCount){
+        guestScore.style.boxShadow= "0 0 0 1px red"
+        homeScore.style.boxShadow = "none"
+    }
+}
+
+function newGame(){
+    homeCount = 0
+    homeScore.innerText = homeCount
+    guestCount = 0
+    guestScore.innerText = guestCount
+    homeScore.style.boxShadow = "none"
+    guestScore.style.boxShadow = "none"
+}
